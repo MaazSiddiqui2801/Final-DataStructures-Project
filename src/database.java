758a277919ffcd4e5d511173c100397443bf8a3d
muhammadanas25 <58306914+muhammadanas25@users.noreply.github.com>
2021-11-29 03:04:12 +0500

    first commit

---
+++ src/database.java
@@ -0,0 +1,22 @@
+public class database {
+    public void fileRead(){
+        // yahan file read karna ka scene hoga from excel file
+    }
+    public void databaseDesign(){
+        // Making 2-d  Array for Province List
+        
+        String [][]arr = new  String[4][1];
+        arr[0][0]= "Sindh";
+        arr[1][0]= "Punjab";
+        arr[2][0]= "Balochistan";
+        arr[3][0]= "KP";//Khyber Pakhtunkhwa
+        arr[4][0]= "FATA";// Federally Administrated Tribal Area
+
+
+
+
+
+
+
+    }
+}
